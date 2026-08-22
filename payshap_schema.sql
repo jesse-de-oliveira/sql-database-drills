a254a5af733221a4fc740617d108a79ad94a73b6
Jesse De Oliveira <jessedeoliveira@edu.unifil.br>
2026-08-22 16:49:01 +0200

    chore: instantiate remaining tables (transactions & ledger_entries)

--- payshap_schema.sql
+++ payshap_schema.sql
@@ -13,3 +13,20 @@ CREATE TABLE natural_keys (
                               is_active BOOLEAN NOT NULL DEFAULT TRUE,
                               key_type VARCHAR(20) NOT NULL
 );
+
+CREATE TABLE transactions (
+                              id UUID PRIMARY KEY,
+                              idempotency_key VARCHAR(100) UNIQUE NOT NULL,
+                              status VARCHAR(20) NOT NULL,
+                              amount NUMERIC(19, 4) NOT NULL,
+                              created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE ledger_entries (
+                                id UUID PRIMARY KEY,
+                                transaction_id UUID NOT NULL REFERENCES transactions(id),
+                                account_id UUID NOT NULL REFERENCES accounts(id),
+                                amount NUMERIC(19, 4) NOT NULL,
+                                transaction_type VARCHAR(6) NOT NULL,
+                                created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
